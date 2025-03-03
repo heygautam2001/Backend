34b692ee62bbe548cb1d06cd6de3cfd9367d0b8d
heygautam2001 <gautamfrankestein@gmail.com>
2025-03-03 15:52:21 +0530

    corrections

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,28 +1,25 @@
-
-
 const asyncHandler = (requestHandler) => {
-    (req,res,next) => {
-      Promise.resolve(requestHandler(req,res,next)).reject((err)=>{
-        return next(err)
-      })
-    }
-}
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).reject((err) => {
+      return next(err);
+    });
+  };
+};
 
-export{asyncHandler}
+export { asyncHandler };
 
-// const asyncHandler = () =>{}
-// const asyncHandler = (func) => ()=>{}
-// const asyncHandler = (fn) => async()=>{}
-
-  // const asyncHandler = (fn) => async(req,req,next) => {
-  //   try{
-  //     await fn(req,res,next)
-
-  //   }catch(error){
-  //     res.status(err.code || 500).json({
-  //       success: false,
-  //       message: error.message
-  //     })
-
-  //   }
-  // }
+// const asyncHandler = () => {};
+// const asyncHandler = (func) => {() => {}
+// };
+// const asyncHandler = (fn) => async () => {};
+   
+// const asyncHandler = (fn) => async (req, req, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
