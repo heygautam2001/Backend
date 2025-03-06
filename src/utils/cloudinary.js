437c9d6b9540bb50460c4618abf20327a83e6726
heygautam2001 <gautamfrankestein@gmail.com>
2025-03-06 12:11:12 +0530

    add middlewares

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,38 @@
+import { v2 as cloudinary } from 'cloudinary'
+import fs from "fs" // file system in buit module in node js
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY ,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async(localFilePath)=>{
+  try {
+    if(!localFilePath) return null
+    //ipload the file on cloudnary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+   })
+
+   //file has been uploaded successfully
+   console.log("file is uploaded on cloudinary" , response.url);
+   return response;
+    
+  } catch (error) {
+    fs.unlinkSync(localFilePath)//remove the file from local storage
+    //remove the locally saved temporary file as the upload operation got failed
+    return null
+
+  }
+}
+export {uploadOnCloudinary}
+//temporary code for understanding
+// cloudinary.v2.uploader
+// .upload("dog.mp4", {
+//   resource_type: "video", 
+//   public_id: "my_dog",
+//   overwrite: true, 
+//   notification_url: "https://mysite.example.com/notify_endpoint"})
+// .then(result=>console.log(result));
+
